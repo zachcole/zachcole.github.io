7c778927ef29694bfeca22857e3f17f195438f31
Zach Cole <zach.cole@getbellhops.com>
2017-09-20 01:21:15 -0400

    adding material ui and bootstrap libraries

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,9 @@ import Interactive from 'react-interactive';
 import { Link } from 'react-router-dom';
 import { Code } from '../styles/style';
 import s from '../styles/home.style';
+import AppBar from 'material-ui/AppBar';
+import Jumbotron from 'react-bootstrap/lib/Jumbotron';
+import Col from 'react-bootstrap/lib/Col';
 
 export default function Home() {
   const repoReadmeLink = text => (
@@ -15,32 +18,16 @@ export default function Home() {
 
   return (
     <div>
-      <p style={s.p}>
-        This is an example single page app built
-        with React and React&nbsp;Router using {' '}
-        <Code>BrowserRouter</Code>. Navigate with the links below and
-        refresh the page or copy/paste the url to test out the redirect
-        functionality deployed to overcome GitHub&nbsp;Pages incompatibility
-        with single page apps (like this one).
-      </p>
-      <p style={s.p}>
-        Please see the {repoReadmeLink('repo readme')} for instructions on how to
-        use this boilerplate to deploy your own single page app using GitHub Pages.
-      </p>
-      <div style={s.pageLinkContainer}>
-        <Interactive
-          as={Link}
-          {...s.link}
-          to="/example"
-        >Example page</Interactive>
-      </div>
-      <div style={s.pageLinkContainer}>
-        <Interactive
-          as={Link}
-          {...s.link}
-          to="/example/two-deep?field1=foo&field2=bar#boom!"
-        >Example two deep with query and hash</Interactive>
-      </div>
+      <AppBar
+        title="Zach Cole"
+        iconClassNameRight="muidocs-icon-navigation-expand-more"
+      />
+      <Col md={6} mdOffset={4}>
+        <Jumbotron>
+          <h1>Coming Soon!</h1>
+          <p>Still under construction...</p>
+        </Jumbotron>
+      </Col>
     </div>
   );
 }
